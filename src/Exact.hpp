2361f16880da13cbb139c267ef7eb010dc5c5ae8
JoD <holkann@gmail.com>
2021-07-16 17:28:42 +0200

    tmp

--- src/Exact.hpp
+++ src/Exact.hpp
@@ -35,3 +35,48 @@ int main_python();
 - optimize (UNSAT and SAT)
  *
  */
+
+/*
+ * - attachConstraint
+- learnConstraint
+- addConstraint
+- addConstraintChecked
+- ILP::normalize
+- addLowerBound
+- handleNewSolution x 2
+- optimize (UNSAT and SAT)
+
+attachConstraint
+
+- learnConstraint
+- - probe
+- - - detectAtMostOne
+- - - - runAtMostOneDetection
+- - - - - inProcess
+- - - - - - presolve
+- - - - - - - solve
+- - - - - - solve
+- - - probeRestart
+- - - - solve
+- - extractCore
+- - - solve
+- - reduceDB
+- - - solve
+- - solve
+- - detectAtMostOne
+- - - runAtMostOneDetection
+- - - - inProcess
+- - addFilteredCuts
+- - - LP::inProcess
+- - - - inProcess
+- - checkFeasiblity x2
+- - - runPropagation() - done
+- - - LP::inProcess
+
+
+
+- AddUnitConstraint
+- - harden
+- - - handleNewSolution
+- - - addLowerBound
+ */
