4fd7340d55d95a8cae349c754f92717c6dcdadef
Test <test@Tests-iMac.local>
2021-07-19 21:49:33 +0200

    compile on OS X

--- src/aux.hpp
+++ src/aux.hpp
@@ -337,6 +337,26 @@ SMALL cast(const LARGE& x) {
   return static_cast<SMALL>(x);
 }
 
+#ifdef __APPLE__
+template <>
+inline int128 abs(const int128& x) {
+  return x < 0 ? -x : x;
+}
+template <>
+inline int128 gcd(const int128& x, const int128& y) {
+  return static_cast<int128>(
+      boost::multiprecision::gcd(boost::multiprecision::int128_t(x), boost::multiprecision::int128_t(y)));
+}
+template <>
+inline unsigned msb(const int128& x) {
+  return boost::multiprecision::msb(boost::multiprecision::uint128_t(x));
+}
+template <>
+inline int128 pow(const int128& x, unsigned y) {
+  return static_cast<int128>(boost::multiprecision::pow(boost::multiprecision::int128_t(x), y));
+}
+#endif
+
 namespace rng {
 
 extern uint32_t seed; /* The seed must be initialized to non-zero */
