69da5a71ba9d5a5813ce34d2601cc282c353de08
JoD <holkann@gmail.com>
2022-07-01 17:48:57 +0200

    update script

--- python/examples/ramsey.py
+++ python/examples/ramsey.py
@@ -19,6 +19,7 @@
 # Looking for Ramsey numbers
 
 import math
+import sys
 
 # Import the exact package, e.g., from PyPI using poetry or pip
 import exact
@@ -27,13 +28,19 @@ import exact
 solver = exact.Exact()
 
 # Fixing Ramsey number instance
-nodes = 24 # size of the graph
-cliques = [4,5]  # size of cliques
+if len(sys.argv)!=5:
+    print("Usage: python3 ramsey.py #nodes cliquesize1 cliquesize2 symcutoff")
+    exit(1)
+nodes = int(sys.argv[1]) # size of the graph
+cliques = [int(sys.argv[2]),int(sys.argv[3])]  # size of cliques
+cutoff = int(sys.argv[4]) # should be less than 64 so that resulting coefficients still fit in long long
+print("Searching for complete bicolored graph with",nodes,"nodes and no blue cliques of size",cliques[0],"and no red cliques of size",cliques[1])
+print("Lex-leader symmetry breaker cutoff:",cutoff)
 
-def toVar(v1,v2):
-    return str(v1)+"_"+str(v2);
+def toVar(v):
+    return str(v[0])+"_"+str(v[1]);
 
-edges = [toVar(i,j) for i in range(0,nodes) for j in range(i+1,nodes)]
+edges = [toVar((i,j)) for i in range(0,nodes) for j in range(i+1,nodes)]
 
 # Add the variables
 for e in edges:
@@ -47,7 +54,7 @@ for n in cliques:
     coefs = [1]*edgesize
 
     while True:
-        vars = [toVar(clique[i],clique[j]) for i in range(0,n) for j in range(i+1,n)]
+        vars = [toVar((clique[i],clique[j])) for i in range(0,n) for j in range(i+1,n)]
         solver.addConstraint(coefs, vars, not flag, 1, flag, edgesize-1)
         index = n-1
         clique[index] += 1
@@ -64,37 +71,42 @@ for n in cliques:
     flag = False
 
 # Lex-leader symmetry breaking
-def swapif(v,i,j):
-    return j if v==i else i if v==j else v
-def image(v1,v2,i,j):
-    i1 = swapif(v1,i,j)
-    i2 = swapif(v2,i,j)
-    return (i1,i2) if i1 <= i2 else (i2,i1)
-
-cutoff = 60 # should be less than 64 so that resulting coefficients still fit in long long
+def swapif(v,swap):
+    return swap[1] if v==swap[0] else swap[0] if v==swap[1] else v
+def image(v,swap):
+    img1 = swapif(v[0],swap)
+    img2 = swapif(v[1],swap)
+    return (img1,img2) if img1 <= img2 else (img2,img1)
 order = [(i,j) for i in range(0,nodes) for j in range(i+1,nodes)]
 orderdict = {order[i]:i for i in range(0,len(order))}
+def addLexLeader(swap1,swap2):
+    images = []
+    for (v1,v2) in order:
+        (v3,v4) = image(image((v1,v2),swap1),swap2)
+        if (v1!=v3 or v2!=v4) and orderdict[(v1,v2)]<orderdict[(v3,v4)]:
+            images += [((v1,v2),(v3,v4))]
+            if len(images)>=cutoff:
+                break
+    coefs = []
+    vars = []
+    c = int(round(pow(2,len(images))))
+    for (v1,v2) in images:
+        vars += [toVar(v1), toVar(v2)]
+        coefs += [-c,c]
+        c //= 2
+    solver.addConstraint(coefs, vars, True, 0, False, 0)
+
 for i in range(0,nodes):
+    print(i)
     for j in range(i+1,nodes):
-        images = []
-        for (v1,v2) in order:
-            (v3,v4) = image(v1,v2,i,j)
-            if (v1!=v3 or v2!=v4) and orderdict[(v1,v2)]<orderdict[(v3,v4)]:
-                images += [(v1,v2,v3,v4)]
-                if len(images)>=cutoff:
-                    break
-        coefs = []
-        vars = []
-        c = int(round(pow(2,len(images))))
-        for (v1,v2,v3,v4) in images:
-            vars += [toVar(v1,v2), toVar(v3,v4)]
-            coefs += [-c,c]
-            c //= 2
-        solver.addConstraint(coefs, vars, True, 0, False, 0)
+        addLexLeader((i,j),(0,0)) # swap only i and j
+        for k in range(i+1,nodes):
+            for l in range(k+1,nodes):
+                addLexLeader((i,j),(k,l))
 
 # Symmetry breaking on color
 if cliques[0]==cliques[1]:
-    solver.addConstraint([1], [toVar(order[0][0],order[0][1])], False, 0, True, 0)
+    solver.addConstraint([1], [toVar(order[0])], False, 0, True, 0)
 
 # Initialize Exact
 solver.init([], [], False, True)
@@ -107,6 +119,8 @@ result = 3
 while result>1:
      # As long as the result is not UNSAT (== 0), the solver is kept running.
      result = solver.run()
+
+solver.printStats()
 
 if result==0:
     assert not solver.hasSolution()
