3994f590673318e3691dbb48c2ccbd15630070b5
JoD <holkann@gmail.com>
2022-09-22 17:23:51 +0200

    better objectives

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -155,15 +155,20 @@ void opb_read(std::istream& in, ILP& ilp) {
   std::vector<bigint> coefs;
   std::vector<IntVar*> vars;
   std::vector<bool> negated;
+  bigint lb;
+  long long lineNr = -1;
   for (std::string line; getline(in, line);) {
+    ++lineNr;
     if (line.empty() || line[0] == '*') continue;
-    for (char& c : line)
+    for (char& c : line) {
       if (c == ';') c = ' ';
+    }
     bool opt_line = line.substr(0, 4) == "min:";
-    bigint lb;
+    lb = 0;
     bool isInequality = false;
     if (opt_line) {
-      line = line.substr(4), assert(first_constraint);
+      assert(first_constraint);
+      line = line.substr(4);
       if (std::all_of(line.begin(), line.end(), isspace)) {
         std::cout << "c WARNING objective function is empty" << std::endl;
         // TODO: global warning routine that also checks for verbosity
@@ -171,9 +176,9 @@ void opb_read(std::istream& in, ILP& ilp) {
     } else {
       size_t eqpos = line.find('=');
       if (eqpos == std::string::npos || eqpos == 0) {
-        throw std::invalid_argument("Incorrect constraint\n" + line);
+        throw std::invalid_argument("Incorrect constraint at line " + std::to_string(lineNr) + ":\n" + line);
       }
-      lb = read_bigint(line, eqpos + 1);
+      lb += read_bigint(line, eqpos + 1);
       isInequality = line[eqpos - 1] == '>';
       line = line.substr(0, eqpos - isInequality);
     }
@@ -184,11 +189,11 @@ void opb_read(std::istream& in, ILP& ilp) {
     std::string tmp;
     while (is >> tmp) tokens.push_back(tmp);
     if (tokens.size() % 2 != 0) {
-      throw std::invalid_argument("No support for non-linear constraints.");
+      throw std::invalid_argument("No support for non-linear constraint at line " + std::to_string(lineNr));
     }
     for (int i = 0; i < (long long)tokens.size(); i += 2) {
       if (find(tokens[i].cbegin(), tokens[i].cend(), 'x') != tokens[i].cend()) {
-        throw std::invalid_argument("No support for non-linear constraints.");
+        throw std::invalid_argument("No support for non-linear constraint at line " + std::to_string(lineNr));
       }
     }
 
@@ -197,16 +202,19 @@ void opb_read(std::istream& in, ILP& ilp) {
     negated.clear();
     for (int i = 0; i < (long long)tokens.size(); i += 2) {
       const std::string& var = tokens[i + 1];
-      if (var.empty() || (var[0] != '~' && var[0] != 'x') || (var[0] == '~' && var[1] != 'x')) {
-        throw std::invalid_argument("Invalid literal token: " + var);
+      if (var.empty()) {
+        throw std::invalid_argument("Invalid literal token " + var + " at line " + std::to_string(lineNr));
+      } else if (var[0] != '~' && var[0] != 'x') {
+        lb -= read_bigint(tokens[i], 0) * read_bigint(var, 0);
+      } else {
+        coefs.emplace_back(read_bigint(tokens[i], 0));
+        negated.emplace_back(var[0] == '~');
+        vars.emplace_back(indexedBoolVar(ilp, var.substr(negated.back() + 1)));
       }
-      coefs.emplace_back(read_bigint(tokens[i], 0));
-      negated.emplace_back(var[0] == '~');
-      vars.emplace_back(indexedBoolVar(ilp, var.substr(negated.back() + 1)));
     }
 
     if (opt_line) {
-      ilp.setObjective(coefs, vars, negated);
+      ilp.setObjective(coefs, vars, negated, 1, -lb);
     } else {
       ilp.addConstraint(coefs, vars, negated, lb, aux::option(!isInequality, lb));
     }
