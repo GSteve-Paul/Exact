adcf97e1cc9636d6d4c04fb837c37e7f6fbdad0e
JoD <holkann@gmail.com>
2023-01-23 17:18:42 +0100

    some macOS learned lessons

--- python/install_package.sh
+++ python/install_package.sh
@@ -12,9 +12,12 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -Dbuild_result=SharedLib -Dbuild_static=OFF 
 make -j $threads
 cp libExact.so exact/libExact.so
 # NOTE: the python/exact folder contains all files necessary for the python package
+# FOR macOS:
+# cp libExact.dylib exact/libExact.dylib
 
 # create and install python module
 python3 -m pip install . -v
+# FOR macOS: when encountering the message that cppyy-backend could not be installed, first install cppyy-cling.
 
 # test on a knapsack example
 python3 examples/knapsack_classic.py
