8946153d97021c96a95b13341313eb956702b6c9
JoD <holkann@gmail.com>
2023-03-22 22:25:29 +0100

    more comments in logging.py

--- python/examples/logging.py
+++ python/examples/logging.py
@@ -1,15 +1,18 @@
-# Prove the clausal decomposition of x+y+z >= 2
+# Prove that x | w is implied by x + y - z >= 1 & w - y + z >= 0
 
 import sys
 import math
 
-# Import the exact package, e.g., from PyPI using poetry or pip
+# Import the exact package
 import exact
 
 # Create an Exact solver instance
 solver = exact.Exact()
-solver.setOption("proof-log","/tmp/logging_example") # NOTE: should be set before calling init(), and init() should be called before adding constraints
-# Proof logging only works for pure satisfiability problems without the following options:
+# Activate the proof logging option by passing a filepath. This will generate a .formula and .proof file, which can be
+# verified by VeriPB (https://github.com/StephanGocht/VeriPB)
+# NOTE: the proof logging option should be activated BEFORE calling init(), and init() should be called BEFORE adding constraints
+solver.setOption("proof-log","/tmp/logging_example")
+# Proof logging only works for pure satisfiability problems with the following options:
 solver.setOption("inp-purelits", "0")
 solver.setOption("inp-dombreaklim", "0")
 
@@ -18,7 +21,6 @@ solver.init([], [])
 
 # Add the variables
 variables = ["x","y","z","w"]
-
 for v in variables:
     solver.addVariable(v, 0, 1)
 
@@ -39,3 +41,6 @@ solver.setAssumption("x", [0])
 solver.setAssumption("w", [0])
 state = solver.runFull(False)
 assert(state==2) # inconsistent with assumptions
+
+# now you can run VeriPB on the generated files, e.g.:
+# veripb /tmp/logging_example.formula /tmp/logging_example.proof
