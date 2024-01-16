cb6ec0f9c843c974cc893e095976b27c7b3326dd
JoD <holkann@gmail.com>
2024-01-16 13:03:52 +0100

    add test script

---
+++ test/check_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# usage: ../test/run_all.sh 3 "testruns" /home/jod/workspace/exact-dev/build_testruns/Exact
+
+logfolder="/tmp/Exact/$1"
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+declare -a arr_configs=(
+"simple"
+"arbitrary"
+"noproof"
+"mindiv"
+"rto"
+"slack+1"
+)
+
+for idx in "${!arr_configs[@]}"; do
+    config=${arr_configs[$idx]}
+    mkdir -p `dirname $logfolder/$config.txt`
+    echo "*** $config ***"
+    tail -n 2 $logfolder/$config.txt
+done
