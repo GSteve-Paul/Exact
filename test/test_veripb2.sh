7a947d31f6eec7b0fb2765fb7934135016d2b68b
JoD <holkann@gmail.com>
2024-06-24 19:46:48 +0200

    update test script

--- test/test_veripb2.sh
+++ test/test_veripb2.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
 
+timelim=$1
 ctr=0
 
 for file in /home/jod/workspace/exact/test/instances/opb/dec/*
 do
   ctr=$(( ctr + 1 ))
   echo "*** $ctr $file ***"
-  /home/jod/workspace/exact/build_profile/Exact $file --timeout=1 --proof-log=/tmp/out$ctr --proof-assumptions=0 --verbosity=0 --print-uniform=0
+  /home/jod/workspace/exact/build_profile/Exact $file --timeout=$timelim --proof-log=/tmp/out$ctr --proof-assumptions=0 --verbosity=0 --print-uniform=0
   sleep 0.1
   echo ""
   veripb $file /tmp/out$ctr.proof --arbitraryPrecision --no-requireUnsat
